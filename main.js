8582f719e5976f4ad4b812192decefd97c425678
Ryan Provenzano <ryan.provenz@gmail.com>
2022-08-12 16:03:54 -0400

    playRound function done; plays a single round

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
   let randomNum = Math.floor(Math.random() * 3);
   switch (randomNum) {
@@ -8,7 +9,7 @@ function getComputerChoice(){
     case 2:
       return "Scissors";
   }
-};
+}
 
 function getUserChoice(){
   let choice = "";
@@ -21,8 +22,36 @@ function getUserChoice(){
         invalidChoice = false;
       }
   }
-  return choice;
+  return choice[0].toUpperCase() + choice.slice(1).toLowerCase(); //Format of string will be "Rock", "Paper", or "Scissors"
+}
+
+
+
+function playRound(playerSelection, computerSelection){
+  let roundResult = 0; // 0 means tie, -1 means loss, 1 means Win;
+  if (playerSelection === computerSelection) {
+    return [roundResult, `You Tied! You both selected ${playerSelection}`]
+  } else {
+
+      switch (playerSelection){
+        case "Rock":
+         roundResult = (computerSelection === "Paper")? -1: 1;
+         break;
+        case "Paper":
+         roundResult = (computerSelection === "Scissors")? -1: 1;
+         break;
+        case "Scissors":
+         roundResult = (computerSelection === "Rock")? -1: 1;
+         break;
+       }
+
+  }
+  return (roundResult == 1)?
+    [roundResult, `You won! ${playerSelection} beats ${computerSelection}`] :
+    [roundResult, `You lost! ${computerSelection} beats ${playerSelection}`];
+
 }
 
 let computerSelection = getComputerChoice();
 let userSelection = getUserChoice();
+console.log(playRound(userSelection, computerSelection));
