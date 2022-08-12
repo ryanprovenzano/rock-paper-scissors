1e1b3173b4d9942c8f286dbc08f29a88f988a375
Ryan Provenzano <ryan.provenz@gmail.com>
2022-08-12 15:13:37 -0400

    functions written for getting computer's selection and user's selection

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Hello");
+function getComputerChoice(){
+  let randomNum = Math.floor(Math.random() * 3);
+  switch (randomNum) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+};
+
+function getUserChoice(){
+  let choice = "";
+  invalidChoice = true;
+  while(invalidChoice){
+    choice = prompt("Select your hand:");
+    choiceLower = choice.toLowerCase();
+    if (choiceLower === "rock" || choiceLower === "paper"
+        || choiceLower === "scissors") {
+        invalidChoice = false;
+      }
+  }
+  return choice;
+}
+
+let computerSelection = getComputerChoice();
+let userSelection = getUserChoice();
